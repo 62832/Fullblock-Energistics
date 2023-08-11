93b776be7331e4da70be4020e949323a13bd29b0
90 <hi@90.gripe>
2023-08-11 16:28:04 +0100

    Fix client-server breakage when using conversion monitors twice

--- common/src/main/java/gripe/_90/fulleng/mixin/PartLeftClickPacketMixin.java
+++ common/src/main/java/gripe/_90/fulleng/mixin/PartLeftClickPacketMixin.java
@@ -1,7 +1,5 @@
 package gripe._90.fulleng.mixin;
 
-import java.io.IOException;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -23,13 +21,12 @@ public abstract class PartLeftClickPacketMixin {
     @Shadow(remap = false)
     private boolean alternateUseMode;
 
+    @SuppressWarnings("resource")
     @Inject(method = "serverPacketData", at = @At("TAIL"), remap = false)
-    private void handleConversionMonitorClick(ServerPlayer player, CallbackInfo ci) throws IOException {
-        try (var level = player.level()) {
-            if (level.getBlockEntity(hitResult.getBlockPos()) instanceof ConversionMonitorBlockEntity monitor) {
-                if (hitResult.getDirection().equals(monitor.getFront())) {
-                    monitor.extractItem(player, alternateUseMode);
-                }
+    private void handleConversionMonitorClick(ServerPlayer player, CallbackInfo ci) {
+        if (player.level().getBlockEntity(hitResult.getBlockPos()) instanceof ConversionMonitorBlockEntity monitor) {
+            if (hitResult.getDirection().equals(monitor.getFront())) {
+                monitor.extractItem(player, alternateUseMode);
             }
         }
     }
