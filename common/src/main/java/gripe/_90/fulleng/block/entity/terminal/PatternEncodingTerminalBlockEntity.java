9470eaa622aafc4fe12e5fe6acdd00d1a5063555
90 <hi@90.gripe>
2023-08-31 18:08:21 +0100

    Fix inheritance issue for pattern encoding terminal

--- common/src/main/java/gripe/_90/fulleng/block/entity/terminal/PatternEncodingTerminalBlockEntity.java
+++ common/src/main/java/gripe/_90/fulleng/block/entity/terminal/PatternEncodingTerminalBlockEntity.java
@@ -2,6 +2,8 @@ package gripe._90.fulleng.block.entity.terminal;
 
 import java.util.List;
 
+import org.jetbrains.annotations.Nullable;
+
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.inventory.MenuType;
@@ -50,6 +52,12 @@ public class PatternEncodingTerminalBlockEntity extends StorageTerminalBlockEnti
     @Override
     public PatternEncodingLogic getLogic() {
         return logic;
+    }
+
+    @Nullable
+    @Override
+    public Level getLevel() {
+        return super.getLevel();
     }
 
     @Override
